b0d97cba6df7bfdf45a91c4a9bd6cc7b33826d64
DorinAfg <Dorinafg2@gmail.com>
2025-01-26 16:17:57 +0200

    FIX BUG

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import subprocess
 
+#'start' and 'cmd' open windows of CMD
+#'/K' to keep it open
 for i in range(2):
     subprocess.Popen(['start', 'cmd', '/K', 'python', 'Client.py'], shell=True)
